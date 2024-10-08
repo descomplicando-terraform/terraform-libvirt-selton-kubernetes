f82506c7ed792a02d64b4d67d902d640b0f0b8aa
Selton <sl1586321@gmail.com>
2024-10-08 12:31:56 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,65 @@
+resource "libvirt_volume" "ubuntu-kubernetes" {
+  count  = length(groundwork_libvirt.var.nodes)
+  name   = "ubuntuqcow-${var.nodes[count.index]}"
+  pool   = libvirt_pool.kubernetes.name
+  source = var.img
+  format = "qcow2"
+}
+
+data "template_file" "user_data" {
+  count    = length(var.nodes)
+  template = <<EOF
+#cloud-config
+ssh_pwauth: True
+users:
+  - name: ${nodes[count.index]}
+    lock_passwd: false
+    plain_text_passwd: ${var.nodes[count.index]} 
+    sudo: ALL=(ALL) NOPASSWD:ALL
+EOF
+
+  vars = {
+    hostname = "${var.nodes[count.index]}"
+  }
+}
+
+resource "libvirt_cloudinit_disk" "commoninit" {
+  count = length(var.nodes)
+  name  = "${var.nodes[count.index]}-commoninit.iso"
+  pool  = libvirt_pool.kubernetes.name
+  user_data = template.user_data[count.index]
+}
+
+resource "libvirt_domain" "kubernetes" {
+  count  = length(var.nodes)
+  name   = var.nodes[count.index]
+  memory = var.memory
+  vcpu   = var.vcpus
+
+  cloudinit = libvirt_cloudinit_disk.commoninit[count.index].id
+
+  network_interface {
+    network_name   = libvirt_network.kubernetes-network[count.index].name
+    wait_for_lease = true
+    hostname       = var.nodes[count.index]
+  }
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_port = "1"
+    target_type = "virtio"
+  }
+
+  disk {
+    volume_id = libvirt_volume.ubuntu-kubernetes[count.index].id
+  }
+
+  qemu_agent = true
+
+}
