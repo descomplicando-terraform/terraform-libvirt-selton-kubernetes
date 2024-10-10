10da35f25550727dc0c7279abcf3a900d41b602e
Selton <sl1586321@gmail.com>
2024-10-10 17:39:20 -0300

    remove lint gnore

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# tflint-ignore: terraform_module_pinned_source
 module "groundwork" {
   source = "git@github.com:descomplicando-terraform/terraform-libvirt-selton-groundwork.git?ref=0.0.1"
   pool   = var.pool
