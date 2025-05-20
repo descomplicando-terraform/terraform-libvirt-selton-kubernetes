89c76199e19d066f1a82f0e30fb0f17a47e1f369
seltonlucas <41970465+seltonlucas@users.noreply.github.com>
2025-05-20 20:44:32 -0300

    Update libvirt version

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.7.6"
+      version = "0.8.3"
     }
   }
   required_version = ">= 1.9.0"
