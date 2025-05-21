9428cf7c27a51ad512d06ef21fc929bfb06a0d49
osm_user <osm_user@mydomain.com>
2025-05-21 19:27:22 -0300

    Revert "Update libvirt version"
    This reverts commit 89c76199e19d066f1a82f0e30fb0f17a47e1f369.

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.8.3"
+      version = "0.7.6"
     }
   }
   required_version = ">= 1.9.0"
